496459a07528c3deb7b77883ab1afd09fff9261a
Alex Xu <alex.xu@amd.com>
2025-03-10 13:46:28 -0400

    add OS version variables

--- docs/conf.py
+++ docs/conf.py
@@ -13,6 +13,28 @@ html_title = project
 author = "Advanced Micro Devices, Inc."
 copyright = "Copyright (c) 2024 Advanced Micro Devices, Inc. All rights reserved."
 
+# Supported linux version numbers
+ubuntu_version_numbers = [('24.04', 'noble'), ('22.04', 'jammy')]
+debian_version_numbers = [('12', 'jammy')]
+rhel_release_version_numbers = ['9', '8']
+rhel_version_numbers = ['9.5', '9.4', '8.10']
+sles_version_numbers = ['15.6', '15.5']
+ol_release_version_numbers = ['8']
+ol_version_numbers = [('8.10')]
+azl_version_numbers = ['3.0']
+
+html_context = {
+    "ubuntu_version_numbers" : ubuntu_version_numbers,
+    "debian_version_numbers" : debian_version_numbers,
+    "sles_version_numbers" : sles_version_numbers,
+    "rhel_release_version_numbers" : rhel_release_version_numbers,
+    "rhel_version_numbers" : rhel_version_numbers,
+    "ol_release_version_numbers" : ol_release_version_numbers,
+    "ol_version_numbers" : ol_version_numbers,
+    "azl_version_numbers": azl_version_numbers
+}
+
+
 # Required settings
 html_theme = "rocm_docs_theme"
 html_theme_options = {
