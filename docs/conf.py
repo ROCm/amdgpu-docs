8b57341476f8c4686ccf98e1ddbb7761cd98ea12
yugang-amd <yugang.wang@amd.com>
2025-08-15 14:13:29 -0400

    Update versions

--- docs/conf.py
+++ docs/conf.py
@@ -8,23 +8,24 @@ if os.environ.get("READTHEDOCS", "") == "True":
 project = "Data Center GPU driver"
 
 version = "1.0.0"
-rocm_version = '6.4.3'
-rocm_directory_version = '6.4.3' # in 6.0 rocm was located in /opt/rocm-6.0.0
-amdgpu_version = '6.4.3' # directory in https://repo.radeon.com/rocm/apt/ and https://repo.radeon.com/amdgpu-install/
+rocm_version = '7.0.0'
+rocm_directory_version = '7.0.0' # in 6.0 rocm was located in /opt/rocm-6.0.0
+amdgpu_version = '7.0.0' # directory in https://repo.radeon.com/rocm/apt/ and https://repo.radeon.com/amdgpu-install/
 release = version
 html_title = project
 author = "Advanced Micro Devices, Inc."
-copyright = "Copyright (c) 2024 Advanced Micro Devices, Inc. All rights reserved."
+copyright = "Copyright (c) 2025 Advanced Micro Devices, Inc. All rights reserved."
 
 # Supported linux version numbers
 ubuntu_version_numbers = [('24.04', 'noble'), ('22.04', 'jammy')]
 debian_version_numbers = [('12', 'jammy')]
-rhel_release_version_numbers = ['9', '8']
-rhel_version_numbers = ['9.6', '9.4', '8.10']
+rhel_release_version_numbers = ['10', '9', '8']
+rhel_version_numbers = ['10.0', '9.6', '9.4', '8.10']
 sles_version_numbers = ['15.7', '15.6']
-ol_release_version_numbers = ['9', '8']
-ol_version_numbers = ['9.6', '8.10']
+ol_release_version_numbers = ['10', '9', '8']
+ol_version_numbers = ['10.0', '9.6', '8.10']
 azl_version_numbers = ['3.0']
+rl_version_numbers = ['9.6', '9.4']
 
 html_context = {
     "ubuntu_version_numbers" : ubuntu_version_numbers,
@@ -34,7 +35,8 @@ html_context = {
     "rhel_version_numbers" : rhel_version_numbers,
     "ol_release_version_numbers" : ol_release_version_numbers,
     "ol_version_numbers" : ol_version_numbers,
-    "azl_version_numbers": azl_version_numbers
+    "azl_version_numbers": azl_version_numbers,
+    "rl_version_numbers" : rl_version_numbers
 }
 
 
