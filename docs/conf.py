6fea17baf4f9a4377d11f7ea0b1c574bc418c86a
yugang-amd <yugang.wang@amd.com>
2025-08-20 12:24:13 -0400

    Update Debian repo setup commands

--- docs/conf.py
+++ docs/conf.py
@@ -11,6 +11,7 @@ version = "1.0.0"
 rocm_version = '7.0.0'
 rocm_directory_version = '7.0.0' # in 6.0 rocm was located in /opt/rocm-6.0.0
 amdgpu_version = '7.0.0' # directory in https://repo.radeon.com/rocm/apt/ and https://repo.radeon.com/amdgpu-install/
+amdgpu_url_version = '30.10'
 release = version
 html_title = project
 author = "Advanced Micro Devices, Inc."
