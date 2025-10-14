15bb76eb464ecdee27501dd7c599412ec3d2a715
yugang-amd <yugang.wang@amd.com>
2025-10-14 15:16:51 -0400

    7.1.0 release updates

--- docs/conf.py
+++ docs/conf.py
@@ -8,8 +8,8 @@ if os.environ.get("READTHEDOCS", "") == "True":
 project = "AMD GPU Driver (amdgpu)"
 
 version = "1.0.0"
-rocm_version = '7.0'
-rocm_directory_version = '7.0.2' # in 6.0 rocm was located in /opt/rocm-6.0.0
+rocm_version = '7.1'
+rocm_directory_version = '7.1.0' # in 6.0 rocm was located in /opt/rocm-6.0.0
 amdgpu_version = '30.10.2' # directory in https://repo.radeon.com/rocm/apt/ and https://repo.radeon.com/amdgpu-install/
 amdgpu_url_version = '30.10.2'
 release = version
